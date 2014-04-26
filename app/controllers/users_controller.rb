1672a0a91b083ee01b2bfb03117b88eb0b4c7ca6
AnNOtis <otis@hdtech.com.tw>
2014-04-27 02:26:59 +0800

    constrain user signed in for microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,9 +54,7 @@ class UsersController < ApplicationController
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def signed_in_user
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
-  end
+  
 
   def correct_user
     @user = User.find(params[:id])
