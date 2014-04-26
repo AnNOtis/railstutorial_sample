043dba3121c0fced3442c36ad2ed7f47a61d5da0
AnNOtis <otis@hdtech.com.tw>
2014-04-27 02:17:33 +0800

    add micropost model

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,10 +4,21 @@ class MicropostsController < ApplicationController
 		
 	end
 	def create
-		
+		@micropost = User.microposts.build(micropost_params)
+		if @micropost.save
+			flash[:success] = "Micropost create!"
+			redirect_to root_url
+		else
+			render 'static_pages/home' 
+		end
 	end
 	def destroy
 		
 	end
 
+
+	private
+		def micropost_params
+			params.require(:micropost).permit(:content)
+		end
 end
